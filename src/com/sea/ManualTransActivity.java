b0025247c200b1383e224bba4ff32db3c4664f4b
shainaztamboli <shainaztamboli@gmail.com>
2013-11-30 22:38:11 +0530

    Added category

--- src/com/sea/ManualTransActivity.java
+++ src/com/sea/ManualTransActivity.java
@@ -1,14 +1,19 @@
 package com.sea;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
 public class ManualTransActivity extends Activity {
@@ -19,6 +24,7 @@ public class ManualTransActivity extends Activity {
 	private Dialog dialog;
 	private DatePicker datePicker;
 	private TimePicker timePicker;
+	private Spinner categorySpinner;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,8 @@ public class ManualTransActivity extends Activity {
 		dialog.setTitle("Custom Dialog");
 		datePicker = (DatePicker) dialog.findViewById(R.id.datePicker1);
 		timePicker = (TimePicker) dialog.findViewById(R.id.timePicker1);
+		categorySpinner = (Spinner) findViewById(R.id.SpinnerCategory);
+		addCategory();
 
 		transactionDate = (Button) findViewById(R.id.TransDate);
 		transactionDate.setOnClickListener(new OnClickListener() {
@@ -67,6 +75,16 @@ public class ManualTransActivity extends Activity {
 			}
 		});
 
+	}
+
+	private void addCategory() {
+		Map<String, Integer> categoryMap = DBAdapter.getAllCategories();
+		List<String> list = new ArrayList<String>(categoryMap.keySet());
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, list);
+		dataAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		categorySpinner.setAdapter(dataAdapter);
 	}
 
 	// display current date
